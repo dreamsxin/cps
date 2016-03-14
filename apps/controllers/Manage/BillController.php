c548f5395b76611873400c5be9be58b956f88001
Dreamsxin <dreamsxin@126.com>
2016-03-14 08:48:16 +0800

    Updated

--- apps/controllers/Manage/BillController.php
+++ apps/controllers/Manage/BillController.php
@@ -32,5 +32,33 @@ class BillController extends ControllerBase {
 		$this->view->totals = \Delivery::summatory($keys, $conditions);
 	}
 
+	public function moAction($page = 1) {
+		\Phalcon\Tag::appendTitle('计费Mo信息');
+		$this->view->labels = \Mo::labels();
+		$conditions = array();
+		$reference = $this->request->get('reference');
+		if ($reference) {
+			$conditions['reference'] = $reference;
+		}
+
+		$data = \Mo::find(array(
+			$conditions,
+			'sort' => array('_id' => -1)
+		));
+
+		$paginator = new \Phalcon\Paginator\Adapter\Model(
+				array(
+					"data" => $data,
+					"limit" => 50,
+					"page" => $page
+				)
+		);
+
+		$this->view->page = $paginator->getPaginate();
+
+		$keys = array('enduserprice' => 0);
+		$this->view->totals = \Delivery::summatory($keys, $conditions);
+	}
+
 }
 
