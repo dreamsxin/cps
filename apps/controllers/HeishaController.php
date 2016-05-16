9b67f28768b6b8ab6c6c846d18c4359c5464000b
Dreamsxin <dreamsxin@126.com>
2016-05-16 16:08:21 +0800

    Updated

--- apps/controllers/HeishaController.php
+++ apps/controllers/HeishaController.php
@@ -23,15 +23,25 @@ class HeishaController extends \Phalcon\Mvc\Controller {
 
 	public function drAction() {
 		if ($this->request->isPost()) {
+			$data = $this->request->getPost();
+			if (empty($data)) {
+				$this->response->setStatusCode(400, 'Bad Params');
+				$this->response->setContent("202")->send();exit;
+			}
 			$dn = new \HeiShaDr();
-			$dn->assign($this->request->getPost());
+			$dn->assign($data);
 			if (!$dn->save()) {
 				$this->response->setStatusCode(500, 'Server Error');
 				$this->response->setContent("500")->send();exit;
 			}
 		} else if ($this->request->isGet()) {
+			$data = $this->request->get();
+			if (empty($data)) {
+				$this->response->setStatusCode(400, 'Bad Params');
+				$this->response->setContent("202")->send();exit;
+			}
 			$dn = new \HeiShaDr();
-			$dn->assign($this->request->get());
+			$dn->assign($data);
 			if (!$dn->save()) {
 				$this->response->setStatusCode(500, 'Server Error');
 				$this->response->setContent("500")->send();exit;
